5b978c8c343aec55d38dcf2ac4f9302d5b9bee6b
mmoc09 <mmoc0910@gmail.com>
2023-02-09 21:03:38 +0700

    add sweetalert

--- .history/src/adminComponents/pages/DiscountPage_20221229152824.js
+++ .history/src/adminComponents/pages/DiscountPage_20221229152824.js
@@ -2,13 +2,21 @@ import React from "react";
 import Box from "../components/Box";
 import AddDiscount from "../components/discounts/AddDiscount";
 import Discounts from "../components/discounts/Discounts";
+import { Button } from "../components/forms";
 import Heading from "../components/Heading";
 
 const DiscountPage = () => {
   return (
     <>
       <div>
-        <Heading heading={"Giảm giá"}></Heading>
+        <Heading heading={"Giảm giá"}>
+          {/* <Button
+            className={"px-5 py-2 rounded-lg bg-[#4c7993] text-white"}
+            // onClick={() => dispatch(setShowAddBanner(true))}
+          >
+            Hoàn thành
+          </Button> */}
+        </Heading>
         <div className="grid grid-cols-10 gap-7">
           <Box className={"col-span-4"}>
             <AddDiscount></AddDiscount>
